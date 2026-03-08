b39807112c3b740f834cfeafb47e9137db9e25f8
Kerry Kidd <kerry@raspikidd.com>
2026-03-08 11:56:47 +0000

    Updated Logo and about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <Header />
-  <div class="bg-amber-50 px-6 py-32 lg:px-8">
+  <!--<div class="bg-amber-50 px-6 py-32 lg:px-8">
     <div class="mx-auto max-w-3xl text-base/7 text-gray-700">
       <h1 class="text-4xl font-semibold tracking-tight text-pink-500 sm:text-6xl">
         Hello, I’m Ange
@@ -43,46 +43,47 @@
             </dt>
           </div>
         </dl>
-      <h2 class="mt-4 text-3xl font-semibold tracking-tight text-sky-900 sm:text-5xl">
-        My Approach
-      </h2>
-      <div class="mt-10 max-w-2xl text-gray-600">
-        <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
-          <div class="relative ml-8 pl-9 text-lg">
-            <dt class="inline text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-                stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+        <h2 class="mt-4 text-3xl font-semibold tracking-tight text-sky-900 sm:text-5xl">
+          My Approach
+        </h2>
+        <div class="mt-10 max-w-2xl text-gray-600">
+          <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+                Clear strategy
+              </dt>
+            </div>
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+                Strong brand identity
+              </dt>
+            </div>
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+                Consistent content
+              </dt>
+            </div>
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
               </svg>
-              Clear strategy
-            </dt>
-          </div>
-          <div class="relative ml-8 pl-9 text-lg">
-            <dt class="inline text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-                stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
-              </svg>
-              Strong brand identity
-            </dt>
-          </div>
-          <div class="relative ml-8 pl-9 text-lg">
-            <dt class="inline text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-                stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
-              </svg>
-              Consistent content
-            </dt>
-          </div>
-          <div class="relative ml-8 pl-9 text-lg">
-            <dt class="inline text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-                stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
-              </svg>
-              Measurable performance
-            </dt>
+                Measurable performance
+              </dt>
+            </div>
           </div>
         </dl>
         <p class="mt-8 text-lg font-medium text-pretty text-gray-700 sm:text-xl/8">
@@ -94,6 +95,151 @@
       </div>
     </div>
   </div>
+  </div> -->
+  <div class="relative bg-amber-50">
+    <section class="max-w-6xl mx-auto px-6 py-16">
+
+  <h1 class="text-4xl font-semibold tracking-tight text-pink-500 sm:text-6xl">
+    Meet the Team
+  </h1>
+
+  <div class="grid md:grid-cols-2 gap-12">
+    <!-- LEFT PAGE -->
+    <div>
+      <h2 class="mt-4 text-3xl font-semibold tracking-tight text-sky-900 sm:text-5xl">
+        Hello I'm Ange
+      </h2>
+
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        I’m a certified social media consultant with over 10 years’ experience supporting small businesses.I’m a 
+        passionate social media manager with 10 years of experience and an SMM Certification from Tech Pixies.
+      </p>
+
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        I help small business owners and marketing consultants simplify social media, making it stress-free, 
+        manageable and effective.
+      </p>
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        I help small business owners and marketing consultants simplify social media, making it stress-free, 
+        manageable and effective.
+      </p>
+      <h3 class="mt-8 text-2xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
+        Who I Work With
+      </h3>
+      <div class="mt-10 max-w-2xl text-gray-600">
+        <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
+          <div class="relative ml-8 pl-9 text-lg">
+            <dt class="inline text-gray-700">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 absolute left-1 top-1 h-6 w-6 text-pink-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" /> 
+              </svg>
+              Small business owners who need a dedicated expert to run their socials
+            </dt>
+          </div>
+          <div class="relative ml-8 pl-9 text-lg">
+            <dt class="inline text-gray-700">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 absolute left-1 top-1 h-6 w-6 text-pink-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" /> 
+              </svg>
+              Business owners who want to learn how to plan, create and schedule their own content
+            </dt>
+          </div>
+          <div class="relative ml-8 pl-9 text-lg">
+            <dt class="inline text-gray-700">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 absolute left-1 top-1 h-6 w-6 text-pink-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" /> 
+              </svg>
+              Marketing consultants who need support with strategy and execution
+            </dt>
+          </div>
+        </dl>
+        <h3 class="mt-8 text-2xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
+          My Approach
+        </h3>
+        <div class="mt-10 max-w-2xl text-gray-600">
+          <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+                Clear strategy
+              </dt>
+            </div>
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+                Strong brand identity
+              </dt>
+            </div>
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+                Consistent content
+              </dt>
+            </div>
+            <div class="relative ml-8 pl-9 text-lg">
+              <dt class="inline text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+                  stroke-width="1.5" stroke="currentColor" class="absolute left-1 top-1 h-6 w-5 text-pink-500">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+              </svg>
+                Measurable performance
+              </dt>
+            </div>
+          </dl>
+        <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+          I believe branding should feel like a cup of coffee and a chat, not a corporate boardroom.
+        </p>
+        <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+          Clear strategy. Strong visuals. Confident messaging. All tailored to your business.
+        </p>
+      </div>
+    </div>
+    </div>
+
+    <div>
+      <h2 class="mt-4 text-3xl font-semibold tracking-tight text-sky-900 sm:text-5xl">
+        Hello there. I’m Bob. 🐾
+      </h2>
+
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        I’m Ange’s right hand man, trusted companion, and official supporter of all things wellbeing. If you book 
+        Ange, you’re booking me too. I like to think of myself as an essential part of the team.
+      </p>
+
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        I’m a very distinguished gentleman with a strong focus on workplace morale. My speciality is wellbeing 
+        breaks. I firmly believe every office benefits from a proper stretch of the legs and a breath of fresh air, 
+        so I’m always ready to lead a lunchtime walk around the North Inch.
+      </p>
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        I’m also highly trained in cuddle therapy. It’s amazing what a quick cuddle, a wagging tail, and a 
+        friendly face can do for the mood of the day.
+      </p>
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        Office banter, you’ll usually find me pranking office mates with placing my favourite toy, my mini pink 
+        sparkly duck on their laps to see how long it takes them to notice.  I consider my duck both stylish and 
+        extremely fun.
+      </p>
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        All in all, I’m proud to be Ange’s loyal assistant, morale booster, and wellbeing advocate.
+      </p>
+      <p class="mt-8 text-md text-pretty font-medium text-gray-700 sm:text-lg/8">
+        In summary, I’m an all round g'boy. 🐾
+      </p>
+    </div>
+
+  </div>
+
+</section>
   </div>
   <newsletter-form />
   <Footer />
