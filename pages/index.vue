faa90096fe9906c45d37f2dbb84a967b9fa8174c
Kerry Kidd <kerry@raspikidd.com>
2026-03-08 12:42:44 +0000

    Updated homepage header

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,49 @@
 <template>
-  <Header />
   <div class="relative bg-pink-500">
+    <header class="absolute inset-x-0 top-0 z-50">
+      <div class="mx-auto max-w-7xl">
+        <div class="px-6 pt-6 lg:max-w-2xl lg:pr-0 lg:pl-8">
+          <nav class=" flex items-center justify-between lg:justify-start" aria-label="Global">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Ange Baker Social Media Consultant</span>
+              <img alt="Ange Baker Social Media Consultant" class="h-8 w-auto" src="/public/Logo.jpeg" />
+            </a>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-white lg:hidden" @click="mobileMenuOpen = true">
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon class="size-6" aria-hidden="true" />
+            </button>
+            <div class="hidden lg:ml-12 lg:flex lg:gap-x-14">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :target="item.target" class="text-sm/6 font-semibold text-white">{{ item.name }}</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <div class="fixed inset-0 z-50"></div>
+        <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-pink-500 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div class="flex items-center justify-between">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Ange Baker Social Media Consultant</span>
+              <img class="h-8 w-auto" src="/public/Logo.jpeg" alt="" />
+            </a>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="size-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-pink-200">{{ item.name }}</a>
+              </div>
+              <div class="py-6">
+                <a href="https://calendly.com/angebakersmc/30min" target="_blank" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-pink-200">Book a Call</a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
     <div class="mx-auto max-w-7xl">
         <div class="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
           <svg class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-pink-500 lg:block" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
@@ -121,6 +164,18 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+//import { Dialog, DialogPanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+const navigation = [
+  { name: 'Services', href: '/services' },
+  { name: 'About', href: '/about' },
+  { name: 'Contact Us', href: '/contact' },
+  { name: 'Book a Call', href: 'https://calendly.com/angebakersmc/30min', target: '_blank', },
+]
+
+const mobileMenuOpen = ref(false)
   useHead({
     title: 'Social Media Consultant for Small Businesses | Ange Baker',
     meta: [
